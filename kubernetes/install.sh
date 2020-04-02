35d3d7777a31cc6374e73ae17efe4529c289751e
Christian R. Vozar <christianvozar@gmail.com>
2020-04-02 15:38:43 -0600

    install: kpt

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -4,3 +4,11 @@ if command -v kubectl >/dev/null 2>&1; then
 else
 	brew update && brew upgrade kubenetes-cli
 fi
+
+if command -v kpt >/dev/null 2>&1; then
+	brew tap GoogleContainerTools/kpt https://github.com/GoogleContainerTools/kpt.git
+	brew install kpt
+else
+	brew update && brew upgrade kpt
+fi
+
